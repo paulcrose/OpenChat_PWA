33a83391fe8122f9cd49082d3dc7031ac15e1eca
paulcrose <61593103+paulcrose@users.noreply.github.com>
2021-06-08 19:22:31 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -309,7 +309,7 @@ function incomingCall(name) {
     return new Promise((resolve) => {
         acceptCallButton.onclick = function() {
             background.classList.add(hide);
-            remoteVideo.classList.remove(hide);
+            incomingCallModal.classList.add(hide);
             resolve(true);
         }
 
